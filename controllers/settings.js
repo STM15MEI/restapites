9f53a91d75c5288e64128616d3fb05ecdac3e199
IchanZX1 <klompokalmas07@gmail.com>
2024-05-09 10:21:36 +0700

    tes api

---
+++ controllers/settings.js
@@ -0,0 +1,13 @@
+global.author = "IchanDev";
+global.key_free = ["freeapikey"];
+global.key_premium = [];
+global.limit_free = "30";
+global.limit_premium = "99999";
+global.dateLimit = "Juli, 01 2023",
+  global.mongo_Db = "mongodb+srv://mongodb-bot:IchanzxOfc670@cluster0.ummvuw7.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "chanzxcoderid@gmail.com";
+global.my_email_password = "fjakkuncqitohmzv";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 3001
