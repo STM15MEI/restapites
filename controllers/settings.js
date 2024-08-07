b9ba63daad810bddf3f1e298406835a86ffd8726
STM15MEI <161107028+STM15MEI@users.noreply.github.com>
2024-08-07 07:37:49 +0700

    Add files via upload

---
+++ controllers/settings.js
@@ -0,0 +1,13 @@
+global.author = "OreDigital";
+global.key_free = ["freeapikey"];
+global.key_premium = [];
+global.limit_free = "30";
+global.limit_premium = "99999";
+global.dateLimit = "Juli, 01 2023",
+  global.mongo_Db = "mongodb+srv://OjiOffc:OjiOffc123@data-ore.9forrcw.mongodb.net/?retryWrites=true&w=majority&appName=Data-Ore";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "koltinkooko@gmail.com";
+global.my_email_password = "pjnm piav nqko zuub";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 3001
